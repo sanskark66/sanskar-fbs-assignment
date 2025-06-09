d8f760fb615bbbe11cc136b603d85a9b65918689
sanskark66 <khedkarsanskar7117@gmail.com>
2025-06-10 01:12:42 +0530

    Add files via upload

---
+++ c programming assignment/A2/Age specifier.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	printf("enter a age=",age);
+	scanf("%d", &age);
+	if(age<12){
+		printf("child");
+	} else if(age<12 && age<19){
+		printf("teenager");
+	} else if(age>20 && age<59){
+		printf("adult");
+	} else if(age>60){
+		printf("senior");
+	}
+}
